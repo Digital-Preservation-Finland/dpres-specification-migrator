0200c76c0325bbbf121d3e54ed97dcf6fc6ef343
Johan Kylander <johan.kylander@csc.fi>
2018-06-14 10:59:16 +0300

    KDKPAS-1672 Remove scripts_list from setup.py

--- setup.py
+++ setup.py
@@ -2,32 +2,19 @@
 Install dpres-specification-migrator
 """
 
-import os
 from setuptools import setup, find_packages
 
 
-def scripts_list():
-    """Return list of command line tools from package scripts"""
-    scripts = []
-    for modulename in os.listdir('dpres_specification_migrator/scripts'):
-        if modulename == '__init__.py':
-            continue
-        if not modulename.endswith('.py'):
-            continue
-        modulename = modulename.replace('.py', '')
-        scriptname = modulename.replace('_', '-')
-        scripts.append('%s = dpres_specification_migrator.scripts.%s:main' % (scriptname, modulename))
-    print scripts
-    return scripts
-
-
 def main():
-        """Install dpres-specification-migrator"""
-        setup(
-            name='dpres-specification-migrator',
-            packages=find_packages(exclude=['tests', 'tests.*']),
-            version='dev',
-            entry_points={'console_scripts': scripts_list()})
+    """Install dpres-specification-migrator"""
+    setup(
+        name='dpres-specification-migrator',
+        packages=find_packages(exclude=['tests', 'tests.*']),
+        version='dev',
+        entry_points={'console_scripts':
+                      [('transform-mets = '
+                        'dpres_specification_migrator.scripts.'
+                        'transform_mets:main')]})
 
 
 if __name__ == '__main__':
