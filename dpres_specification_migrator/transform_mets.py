18441f062323d5fefcf884007347421498793d7c
Juha Lehtonen <juha.lehtonen@csc.fi>
2021-02-19 22:32:41 +0200

    KDKPAS-2487 Do not make vocabulary change with old specifications.

--- dpres_specification_migrator/transform_mets.py
+++ dpres_specification_migrator/transform_mets.py
@@ -365,12 +365,12 @@ def migrate_mets(root, to_catalog, cur_catalog, contract=None):
                 attr['MDTYPE'] = 'OTHER'
                 attr['OTHERMDTYPE'] = 'MARC'
 
-    for elem in root.xpath(
-            './mets:fileSec/mets:fileGrp/mets:file[@USE='
-            '"no-file-format-validation"]', namespaces=NAMESPACES):
-        attr = elem.attrib
-        if attr['USE'] == 'no-file-format-validation':
-            attr['USE'] = 'fi-preservation-no-file-format-validation'
+        for elem in root.xpath(
+                './mets:fileSec/mets:fileGrp/mets:file[@USE='
+                '"no-file-format-validation"]', namespaces=NAMESPACES):
+            attr = elem.attrib
+            if attr['USE'] == 'no-file-format-validation':
+                attr['USE'] = 'fi-preservation-no-file-format-validation'
 
     elems = []
     for elem in root.xpath('./*'):
